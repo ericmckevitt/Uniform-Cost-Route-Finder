0642d4436211a2902dcbd07cfac1ecc7642beed0
Eric McKevitt <ericmckevitt@csm-wl-dhcp-68-41.mines.edu>
2023-02-16 22:04:38 -0700

    Added file loading

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello World!")
+import os
+import sys
+
+def command_line_inputs():
+    filename, origin, destination = None, None, None
+    try:
+        filename = sys.argv[1]
+        origin = sys.argv[2]
+        destination = sys.argv[3]
+    except IndexError:
+        print('Usage: python main.py filename origin destination')
+        sys.exit(1)
+    return filename, origin, destination
+
+def main():
+    # Clear the terminal
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    filename, origin, destination = command_line_inputs()
+    print('filename: ', filename)
+    print('origin: ', origin)
+    print('destination: ', destination)
+    
+    
+
+
+if __name__ == '__main__':
+    main()
